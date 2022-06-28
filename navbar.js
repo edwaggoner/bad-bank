9321fc2d89e5216652459bac526c36a2371a643d
Ed Waggoner <edwaggoner@gmail.com>
2022-06-27 21:52:15 -0700

    adds hover titles to navbar

--- navbar.js
+++ navbar.js
@@ -3,7 +3,8 @@ function NavBar(){
   // const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
   // const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
-
+  // const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  // const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
   return(
     <>
@@ -20,13 +21,13 @@ function NavBar(){
           <li className="nav-item">
             <a className="nav-link" href="#/login/">Login</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" title="Add money to your account here.">
             <a className="nav-link" href="#/deposit/">Deposit</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" title="Take money out of your account here.">
             <a className="nav-link" href="#/withdraw/">Withdraw</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item" title="View account information for all users.">
             <a className="nav-link" href="#/alldata/">AllData</a>
           </li>
         </ul>
