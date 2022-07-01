39c58a7be6b3f7d0a833acd498cbdfcde402460e
Ed Waggoner <edwaggoner@gmail.com>
2022-06-30 22:43:23 -0700

    almost has validations working

--- createaccount.js
+++ createaccount.js
@@ -4,6 +4,9 @@ function CreateAccount(){
   const [name, setName]         = React.useState('');
   const [email, setEmail]       = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [nameready, setNameready]     = React.useState(false);
+  const [emailready, setEmailready]   = React.useState(false);
+  const [passready, setPassready]     = React.useState(false);
   const ctx = React.useContext(UserContext);
 
   function validate(field, label){
@@ -14,6 +17,59 @@ function CreateAccount(){
       }
       return true;
   }
+
+  function handleNameChanged(e){
+    const value = e.currentTarget.value;
+    setName(value);
+    if (value === '') {
+      setStatus('');
+      setNameready(false);
+      return;
+    }
+    setNameready(true);
+  }
+
+  function handleEmailChanged(e){
+    const value = e.currentTarget.value;
+    setEmail(value);
+    if (value === '') {
+      setStatus('');
+      setEmailready(false);
+      return;
+    }
+    if (!(value.includes('@'))) {
+      setStatus('Your email must include @');
+      setEmailready(false);
+      return;
+    }
+    // Set status to '' so that ...the error msg about including @ does not display
+    setStatus('');
+    setEmailready(true);
+  }
+
+  function handlePasswordChanged(e){
+    const value = e.currentTarget.value;
+    setPassword(value);
+    if (value === '') {
+      setStatus('');
+      setPassready(false);
+      return;
+    }
+    let length = value.length;
+    if (length < 8) {
+      setStatus('Your password must contain at least 8 characters');
+      setPassready(false);
+      return;
+    }
+    setStatus('');
+    setPassready(true);
+    if (nameready === true && passready === true && emailready === false) {
+      setStatus('Your email must include @');
+    }
+  }
+
+
+// if name is OK, email has something but does not include '@', and password is 8 or greater OK, the 'Your email must include @' msg should be displayed.
 
   function handleCreate(){
     console.log(name,email,password);
@@ -31,6 +87,9 @@ function CreateAccount(){
     setEmail('');
     setPassword('');
     setShow(true);
+    setNameready(false);
+    setEmailready(false);
+    setPassready(false);
   }
 
   return (
@@ -41,12 +100,12 @@ function CreateAccount(){
       body={show ? (
               <>
               Name<br/>
-              <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+              <input className="form-control" id="name" placeholder="Enter name" value={name} onChange={handleNameChanged} /><br/>
               Email address<br/>
-              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+              <input type="email" className="form-control" id="email" placeholder="Enter email" value={email} onChange={handleEmailChanged}/><br/>
               Password<br/>
-              <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={handlePasswordChanged}/><br/>
+              <button type="submit" className="btn btn-light" onClick={handleCreate} disabled={!(nameready === true && emailready === true && passready === true)}>Create Account</button>
               </>
             ):(
               <>
